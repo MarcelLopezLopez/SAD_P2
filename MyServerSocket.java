a22767b760b914a31f0386ee246fe4fa6ef19567
MarcelLopezLopez <marcelopez@estudiantat.upc.edu>
2023-11-01 20:23:42 +0100

    MyServerSocekt acabat

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -11,18 +11,22 @@ public class MyServerSocket extends ServerSocket {
         super(port);
     }
 
+    // Retorna el port on es troba el servidor
     public int getLocalPort() {
         return ss.getLocalPort();
     }
 
+    // Retorna true si hi ha algú conectat al servidor
     public boolean isBound(){
         return connect;
     }
 
+    // Retorn true si el servidor esta tancat
     public boolean isClosed(){
         return !connect;
     }
 
+    // Escucha si se realiza una conexión a este socket y la acepta
     public MySocket accept() {
         try {
             connect = true;
